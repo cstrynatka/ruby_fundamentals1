ea7a89b5db4430004bfb94485c35f4c65719822c
Christina <christina@Macintosh.local>
2015-08-18 16:37:26 -0400

    Completed exercise 2 for week_one_day_two assignment

--- exercise1.rb
+++ exercise1.rb
@@ -14,3 +14,19 @@ b = 65
 puts "The value of a is #{a}." #The value of a is 20
 puts "The value of a is #{b}." #The value of a is 65
 puts "The value of a plus b is #{a + b}." #The value of a plus b is 85
+
+puts 2 != 3
+
+puts 55.0 * 1.15 #Calculating a tip at a restaurant
+puts 55.0 * 1.20 #Calculating a really good tip at a restaurant by tipping 20%
+
+puts "hello world, here I am" #String to be added to an integer
+puts "6" #Integer to be added to a string
+
+puts "hello world, here I am #{6}"
+
+puts 45628 * 7839
+
+puts "45628 * #{7839}"
+
+puts (true && false) || (false && true) || !(false && false)
