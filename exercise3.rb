52ac03fe348bf2c9bed4b731c35406199962ea5e
Christina <christina@Macintosh.local>
2015-08-18 17:21:04 -0400

    created a basic ruby program by asking the user basic invasive questions about themself

---
+++ exercise3.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+ first_name = gets.chomp
+puts "Your first name is #{first_name}"
+
+puts "What is your birth year?"
+ birth_year = gets.chomp
+ puts "Your birth year is #{birth_year}"
+
+puts "What did you have for breakfast?"
+ for_breakfast = gets.chomp
+ puts "Your breakfast was cashews #{for_breakfast}"
+
+puts "Is anybody listening?"
+ somebody_might_be_listening = gets.chomp
+ puts "Somebody might be listening #{somebody_might_be_listening}"
+
+puts "Are you eating a banana?"
+ banana = gets.chomp
+ puts "You're eating a banana #{banana}"
