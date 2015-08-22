0ca30b79dac31ec0031037964ccf611ecec57a95
Christina <christina@ChristiasMacAir.lan>
2015-08-21 23:52:40 -0400

    Re-did homework exercise on how to calculate tip on a restaurant meal

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+puts "Was your meal really good today?"
+answer = gets.chomp
+puts "That's because nobody spat in the food"
+puts "How much did your meal cost?"
+answer = gets.chomp
+puts "Holy cow, you spent that much on a meal??"
+answer = gets.chomp
+puts "Okay. In one word, tell me exactly how good the food was"
+answer = gets.chomp
+puts "Based on your answer of " + answer + ", your total meal cost, including tip, should be"
+puts 55 * 1.20 
+
