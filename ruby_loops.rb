42633218a74276ec11116b618c42fc72116753b1
Christina <christina@Christinas-Mac-Air.local>
2015-08-25 15:40:28 -0400

    committed deletion

--- ruby_loops.rb
+++
@@ -1,8 +0,0 @@
-count = sum = 0
-5.times do 
-	count += 1
-	puts "Sum: " + \
-	"1 + 2 + 3 + ... + " + \
-	count.to_s + " = " + \
-	sum.to_s
-end
--- syntax_test_two.rb
+++
@@ -1,5 +0,0 @@
-x = 2
-y = 2
-if x !== y
-	puts "x and y should be, to my reckoning, equal after all"
-end
